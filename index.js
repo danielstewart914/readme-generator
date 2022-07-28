59f37c64aa3c95774a472fa760b7461886be742f
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-28 10:44:52 -0700

    Added output folder and writeToFile function

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// TODO: Include packages needed for this application
+const fs = require( 'fs' );
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// write file 'data' to output folder
+const writeToFile = ( fileName, data ) => {
+
+    fs.writeFile(`./output/${ fileName }`, data, ( error ) => {
+        if ( error ) throw err;
+        console.log( 'The file has been saved!' );
+        } );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
