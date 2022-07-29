0530334e6b118a3cfe9bee55c1b2bcd83cc3cc44
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-29 12:16:25 -0700

    Added questions and primary app functionality

--- index.js
+++ index.js
@@ -1,20 +1,95 @@
 const fs = require( 'fs' );
+const generateMarkdown = require( './utils/generateMarkdown' );
+const inquirer = require( 'inquirer' );
+const { EventEmitter } = require('events');
 
+const questioning = new EventEmitter();
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter your full name',
+        name: 'userName',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub user name?',
+        name: 'gitHub',
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'Please select a license for this project',
+        name: 'license',
+        choices: [ 'MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None' ]
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run for tests?',
+        name: 'tests',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using this repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to this repo?',
+        name: 'contributing',
+    }
+];
 
 // write file 'data' to output folder
 const writeToFile = ( fileName, data ) => {
 
-    fs.writeFile(`./output/${ fileName }`, data, ( error ) => {
-        if ( error ) throw err;
-        console.log( 'The file has been saved!' );
-        } );
-}
+    fs.writeFile( `./output/${ fileName }`, data, error => {
 
-// TODO: Create a function to initialize app
-function init() {}
+        if ( error ) console.log( error );
+        else console.error( 'The file has been generated!' );
+
+    } );
+};
+
+const queryUser = questions => {
+
+    inquirer
+    .prompt( questions )
+    .then(  answers => questioning.emit( 'finished', answers ) )
+    .catch( error  =>  console.error( error ) );
+
+};
+
+const init = () => {
+
+    // when questioning is done write generated markdown to file using answers provided
+    questioning.on( 'finished', answers => writeToFile( 'README.md', generateMarkdown( answers ) ) );
+
+    // query user for info
+    queryUser( questions );
+
+};
 
 // Function call to initialize app
 init();
