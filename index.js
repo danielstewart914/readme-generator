0241f577bdc65d93b53a2032130d845a46704ca0
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-29 12:29:12 -0700

    Moved questions to separate module

--- index.js
+++ index.js
@@ -1,65 +1,12 @@
+// modules
 const fs = require( 'fs' );
-const generateMarkdown = require( './utils/generateMarkdown' );
-const inquirer = require( 'inquirer' );
 const { EventEmitter } = require('events');
+const inquirer = require( 'inquirer' );
+
+const generateMarkdown = require( './utils/generateMarkdown' );
+const questions = require( './utils/questions' );
 
 const questioning = new EventEmitter();
-
-const questions = [
-    {
-        type: 'input',
-        message: 'Please enter your full name',
-        name: 'userName',
-    },
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: 'What is your GitHub user name?',
-        name: 'gitHub',
-    },
-    {
-        type: 'input',
-        message: 'What is the name of your project?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'Please write a short description of your project:',
-        name: 'description',
-    },
-    {
-        type: 'list',
-        message: 'Please select a license for this project',
-        name: 'license',
-        choices: [ 'MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None' ]
-    },
-    {
-        type: 'input',
-        message: 'What command should be run to install dependencies?',
-        name: 'installation',
-        default: 'npm i'
-    },
-    {
-        type: 'input',
-        message: 'What command should be run for tests?',
-        name: 'tests',
-        default: 'npm test'
-    },
-    {
-        type: 'input',
-        message: 'What does the user need to know about using this repo?',
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'What does the user need to know about contributing to this repo?',
-        name: 'contributing',
-    }
-];
 
 // write file 'data' to output folder
 const writeToFile = ( fileName, data ) => {
