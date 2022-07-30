6d3eabb7ced064ee0a8a9bd90e49e9a3a55d949a
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-30 12:29:00 -0700

    Refactored code to use async function for questions

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 // modules
 const fs = require( 'fs' );
-const { EventEmitter } = require('events');
 const inquirer = require( 'inquirer' );
 
 const generateMarkdown = require( './utils/generateMarkdown' );
 const questions = require( './utils/questions' );
-
-const questioning = new EventEmitter();
 
 // write file 'data' to output folder
 const writeToFile = ( fileName, data ) => {
 
     fs.writeFile( `./output/${ fileName }`, data, error => {
 
-        if ( error ) console.log( error );
-        else console.error( 'The file has been generated!' );
+         error ? console.error( error ) : console.log( 'The file has been generated!' );
 
     } );
 };
 
-const queryUser = questions => {
+const queryUser = async questions => {
 
-    inquirer
-    .prompt( questions )
-    .then(  answers => questioning.emit( 'finished', answers ) )
-    .catch( error  =>  console.error( error ) );
+    try {
+
+        const answers = await inquirer.prompt( questions );
+
+        // write generated markdown to file using answers provided
+        writeToFile( 'README.md', generateMarkdown( answers ) );
+
+    } catch ( error ) {
+
+        console.error( error );
+        
+    } 
 
 };
 
 const init = () => {
-
-    // when questioning is done write generated markdown to file using answers provided
-    questioning.on( 'finished', answers => writeToFile( 'README.md', generateMarkdown( answers ) ) );
 
     // query user for info
     queryUser( questions );
